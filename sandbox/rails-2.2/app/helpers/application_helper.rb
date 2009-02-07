fcbcb77efc451844a8a151c70720089c88616269
jplang <jplang@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2009-02-07 11:25:23 +0000

    Moves date and time formatting methods to Redmine::I18n.
    git-svn-id: http://svn.redmine.org/redmine@2386 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- sandbox/rails-2.2/app/helpers/application_helper.rb
+++ sandbox/rails-2.2/app/helpers/application_helper.rb
@@ -90,23 +90,6 @@ module ApplicationHelper
     html_options[:onclick] = "promptToRemote('#{text}', '#{param}', '#{url_for(url)}'); return false;"
     link_to name, {}, html_options
   end
-
-  def format_date(date)
-    return nil unless date
-    # "Setting.date_format.size < 2" is a temporary fix (content of date_format setting changed)
-    @date_format ||= (Setting.date_format.blank? || Setting.date_format.size < 2 ? l(:general_fmt_date) : Setting.date_format)
-    date.strftime(@date_format)
-  end
-
-  def format_time(time, include_date = true)
-    return nil unless time
-    time = time.to_time if time.is_a?(String)
-    zone = User.current.time_zone
-    local = zone ? time.in_time_zone(zone) : (time.utc? ? time.localtime : time)
-    @date_format ||= (Setting.date_format.blank? || Setting.date_format.size < 2 ? l(:general_fmt_date) : Setting.date_format)
-    @time_format ||= (Setting.time_format.blank? ? l(:general_fmt_time) : Setting.time_format)
-    include_date ? local.strftime("#{@date_format} #{@time_format}") : local.strftime(@time_format)
-  end
   
   def format_activity_title(text)
     h(truncate_single_line(text, :length => 100))
--- sandbox/rails-2.2/lib/redmine/i18n.rb
+++ sandbox/rails-2.2/lib/redmine/i18n.rb
@@ -24,6 +24,23 @@ module Redmine
     def ll(*args)
       GLoc.ll(*args)
     end
+
+    def format_date(date)
+      return nil unless date
+      # "Setting.date_format.size < 2" is a temporary fix (content of date_format setting changed)
+      @date_format ||= (Setting.date_format.blank? || Setting.date_format.size < 2 ? l(:general_fmt_date) : Setting.date_format)
+      date.strftime(@date_format)
+    end
+    
+    def format_time(time, include_date = true)
+      return nil unless time
+      time = time.to_time if time.is_a?(String)
+      zone = User.current.time_zone
+      local = zone ? time.in_time_zone(zone) : (time.utc? ? time.localtime : time)
+      @date_format ||= (Setting.date_format.blank? || Setting.date_format.size < 2 ? l(:general_fmt_date) : Setting.date_format)
+      @time_format ||= (Setting.time_format.blank? ? l(:general_fmt_time) : Setting.time_format)
+      include_date ? local.strftime("#{@date_format} #{@time_format}") : local.strftime(@time_format)
+    end
     
     def available_locales
       GLoc.valid_languages.collect(&:to_s).sort
