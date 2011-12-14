svn2github/redmine1

commit 9ba84126196e56e8af547b64a803a5ca4c4ff22a

Role.all broken in migration due to default sort (columns do not exist yet). Use raw SQL instead.
git-svn-id: http://svn.redmine.org/redmine@8182 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/db/migrate/020_add_role_position.rb
@@ -1,7 +1,7 @@
 class AddRolePosition < ActiveRecord::Migration
   def self.up
     add_column :roles, :position, :integer, :default => 1
-    Role.find(:all).each_with_index {|role, i| role.update_attribute(:position, i+1)}
+    Role.update_all("position = (SELECT COUNT(*) FROM #{Role.table_name} r WHERE r.id < id) + 1")
   end
 
   def self.down

commit 17aa619904a050c3eb8de396b4a00e64d8f7056d

Restores migration broken by r8182 and removes default scope on Role (#9800).
git-svn-id: http://svn.redmine.org/redmine@8213 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/app/controllers/roles_controller.rb
@@ -30,7 +30,7 @@ class RolesController < ApplicationController
   def new
     # Prefills the form with 'Non member' role permissions
     @role = Role.new(params[:role] || {:permissions => Role.non_member.permissions})
-    @roles = Role.all
+    @roles = Role.sorted.all
   end
 
   def create
@@ -43,7 +43,7 @@ class RolesController < ApplicationController
       flash[:notice] = l(:notice_successful_create)
       redirect_to :action => 'index'
     else
-      @roles = Role.all
+      @roles = Role.sorted.all
       render :action => 'new'
     end
   end
@@ -70,7 +70,7 @@ class RolesController < ApplicationController
   end
 
   def permissions
-    @roles = Role.find(:all, :order => 'builtin, position')
+    @roles = Role.sorted.all
     @permissions = Redmine::AccessControl.permissions.select { |p| !p.public? }
     if request.post?
       @roles.each do |role|

trunk/app/models/role.rb
@@ -26,7 +26,7 @@ class Role < ActiveRecord::Base
     ['own', :label_issues_visibility_own]
   ]
 
-  default_scope :order => 'builtin, position'
+  named_scope :sorted, {:order => 'builtin, position'}
   named_scope :givable, { :conditions => "builtin = 0", :order => 'position' }
   named_scope :builtin, lambda { |*args|
     compare = 'not' if args.first == true

trunk/db/migrate/020_add_role_position.rb
@@ -1,7 +1,7 @@
 class AddRolePosition < ActiveRecord::Migration
   def self.up
     add_column :roles, :position, :integer, :default => 1
-    Role.update_all("position = (SELECT COUNT(*) FROM #{Role.table_name} r WHERE r.id < id) + 1")
+    Role.all.each_with_index {|role, i| role.update_attribute(:position, i+1)}
   end
 
   def self.down