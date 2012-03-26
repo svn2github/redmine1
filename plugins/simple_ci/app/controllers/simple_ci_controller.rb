33ddddfdf45d9512464eb1e7004cabd33d349135
tmaruyama <tmaruyama@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2012-03-26 02:46:04 +0000

    simple_ci plugin: remove trailing white-spaces from sources
    git-svn-id: http://svn.redmine.org/redmine@9271 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- plugins/simple_ci/app/controllers/simple_ci_controller.rb
+++ plugins/simple_ci/app/controllers/simple_ci_controller.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -23,7 +23,7 @@ class SimpleCiController < ApplicationController
   unloadable
   layout 'base'
   before_filter :find_project, :authorize
-  
+
   def show
     # Build the regular expression used to detect successfull builds
     success_re = Regexp.new(Setting.plugin_simple_ci['success_keyword'].strip, Regexp::IGNORECASE)
@@ -58,9 +58,9 @@ class SimpleCiController < ApplicationController
       flash.now[:error] = 'The feed URL is not defined for this project.'
     end
   end
-  
+
 private
-  def find_project   
+  def find_project
     @project = Project.find(params[:id])
   rescue ActiveRecord::RecordNotFound
     render_404
--- plugins/simple_ci/init.rb
+++ plugins/simple_ci/init.rb
@@ -1,16 +1,16 @@
-# redMine - project management software
-# Copyright (C) 2006-2007  Jean-Philippe Lang
+# Redmine - project management software
+# Copyright (C) 2006-2012  Jean-Philippe Lang
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License
 # as published by the Free Software Foundation; either version 2
 # of the License, or (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -25,7 +25,7 @@ Redmine::Plugin.register :simple_ci do
   author 'Jean-Philippe Lang'
   description 'A generic plugin for integrating per project RSS continuous integration feeds in Redmine.'
   version '1.0'
-  
+
   # The feed_url_custom_field setting is used to hold the id of the project custom field
   # that stores the CI feed url for each project
   settings :default => {'feed_url_custom_field' => 0,
