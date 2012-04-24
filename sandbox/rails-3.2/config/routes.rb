50bb44a7a568ad792e8c2ac51042c94f3709874e
jplang <jplang@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2012-04-24 16:49:47 +0000

    Output a more explicit message when a plugin's routes.rb is broken.
    git-svn-id: http://svn.redmine.org/redmine@9526 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- sandbox/rails-3.2/config/routes.rb
+++ sandbox/rails-3.2/config/routes.rb
@@ -306,7 +306,15 @@ RedmineApp::Application.routes.draw do
 
   get 'robots.txt', :to => 'welcome#robots'
 
-  Dir.glob File.expand_path("plugins/*/config/routes.rb", Rails.root) do |file|
-    instance_eval File.read(file)
+  Dir.glob File.expand_path("plugins/*", Rails.root) do |plugin_dir|
+    file = File.join(plugin_dir, "config/routes.rb")
+    if File.exists?(file)
+      begin
+        instance_eval File.read(file)
+      rescue Exception => e
+        puts "An error occurred while loading the routes definition of #{File.basename(plugin_dir)} plugin (#{file}): #{e.message}."
+        exit 1
+      end
+    end
   end
 end
