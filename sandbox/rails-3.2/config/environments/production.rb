bdb18332231b1e0599243577713a507ae5467b18
tmaruyama <tmaruyama@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2012-04-24 08:04:18 +0000

    remove trailing white-spaces from config/environments/*.rb
    git-svn-id: http://svn.redmine.org/redmine@9522 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- sandbox/rails-3.2/config/environments/production.rb
+++ sandbox/rails-3.2/config/environments/production.rb
@@ -3,7 +3,7 @@ RedmineApp::Application.configure do
   # The production environment is meant for finished, "live" apps.
   # Code is not reloaded between requests
   config.cache_classes = true
-  
+
   #####
   # Customize the default logger (http://ruby-doc.org/core/classes/Logger.html)
   #
@@ -15,18 +15,18 @@ RedmineApp::Application.configure do
   #
   # config.logger = Logger.new(config.log_path, 7, 1048576)
   # config.logger.level = Logger::INFO
-  
+
   # Full error reports are disabled and caching is turned on
   config.action_controller.perform_caching = true
-  
+
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host                  = "http://assets.example.com"
-  
+
   # Disable delivery errors if you bad email addresses should just be ignored
   config.action_mailer.raise_delivery_errors = false
-  
+
   # No email in production log
   config.action_mailer.logger = nil
-  
+
   config.active_support.deprecation = :log
 end
