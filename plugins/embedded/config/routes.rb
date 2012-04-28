d72f90a410c81f0be29452346b1312e08db734bb
jplang <jplang@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2012-04-28 11:10:13 +0000

    Removed plugins from core repository.
    git-svn-id: http://svn.redmine.org/redmine@9562 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- plugins/embedded/config/routes.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'embedded/:id/*path', :controller => 'embedded', :action => 'index'
-end
--- plugins/simple_ci/config/routes.rb
+++
@@ -1,5 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.connect 'projects/:id/simple_ci/show',
-              :controller => 'simple_ci',
-              :action => 'show'
-end
