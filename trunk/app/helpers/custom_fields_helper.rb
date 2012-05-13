svn2github/redmine1

commit 874a695018896d0d49eaa06249adf5cc97b73877

Use content_tag helper.
git-svn-id: http://svn.redmine.org/redmine@9682 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/app/helpers/custom_fields_helper.rb
@@ -55,13 +55,13 @@ module CustomFieldsHelper
       unless custom_field.multiple?
         if custom_field.is_required?
           unless custom_field.default_value.present?
-            blank_option = "<option value=\"\">--- #{l(:actionview_instancetag_blank_option)} ---</option>"
+            blank_option = content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---", :value => '')
           end
         else
-          blank_option = '<option></option>'
+          blank_option = content_tag('option')
         end
       end
-      s = select_tag(field_name, blank_option.html_safe + options_for_select(custom_field.possible_values_options(custom_value.customized), custom_value.value),
+      s = select_tag(field_name, blank_option + options_for_select(custom_field.possible_values_options(custom_value.customized), custom_value.value),
         tag_options.merge(:multiple => custom_field.multiple?))
       if custom_field.multiple?
         s << hidden_field_tag(field_name, '')

commit 09ba2c54fd8f72b6d0e314cee29328849f95f325

Fixes custom_field_tag helper broken by r9682.
git-svn-id: http://svn.redmine.org/redmine@9686 e93f8b46-1217-0410-a6f0-8f06a7374b81

trunk/app/helpers/custom_fields_helper.rb
@@ -51,7 +51,7 @@ module CustomFieldsHelper
     when "bool"
       hidden_field_tag(field_name, '0') + check_box_tag(field_name, '1', custom_value.true?, tag_options)
     when "list"
-      blank_option = ''
+      blank_option = ''.html_safe
       unless custom_field.multiple?
         if custom_field.is_required?
           unless custom_field.default_value.present?