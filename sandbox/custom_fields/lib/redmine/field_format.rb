efb72d3a03738592444387dcf249e46c776f5708
jplang <jplang@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2013-11-24 14:37:04 +0000

    Move edit tag rendering to Redmine::FieldFormat.
    git-svn-id: http://svn.redmine.org/redmine@12333 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- sandbox/custom_fields/lib/redmine/field_format.rb
+++ sandbox/custom_fields/lib/redmine/field_format.rb
@@ -48,6 +48,7 @@ module Redmine
 
     class Base
       include Singleton
+      include Redmine::I18n
 
       class_attribute :format_name
       self.format_name = nil
@@ -132,6 +133,27 @@ module Redmine
         cast_value(custom_field, value, customized)
       end
 
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        view.text_field_tag(tag_name, custom_value.value, options.merge(:id => tag_id))
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        view.text_field_tag(tag_name, value, options.merge(:id => tag_id)) +
+          bulk_clear_tag(view, tag_id, tag_name, custom_field, value)
+      end
+
+      def bulk_clear_tag(view, tag_id, tag_name, custom_field, value)
+        if custom_field.is_required?
+          ''.html_safe
+        else
+          view.content_tag('label',
+            view.check_box_tag(tag_name, '__none__', (value == '__none__'), :id => nil, :data => {:disables => "##{tag_id}"}) + l(:button_clear),
+            :class => 'inline'
+          )
+        end
+      end
+      protected :bulk_clear_tag
+
       def query_filter_options(custom_field, query)
         {:type => :string}
       end
@@ -163,6 +185,16 @@ module Redmine
     class TextFormat < Unbounded
       add 'text'
       self.searchable_supported = true
+
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        view.text_area_tag(tag_name, custom_value.value, options.merge(:id => tag_id, :rows => 3))
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        view.text_area_tag(tag_name, value, options.merge(:id => tag_id, :rows => 3)) +
+          '<br />'.html_safe +
+          bulk_clear_tag(view, tag_id, tag_name, custom_field, value)
+      end
 
       def query_filter_options(custom_field, query)
         {:type => :text}
@@ -227,6 +259,17 @@ module Redmine
         end
       end
 
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        view.text_field_tag(tag_name, custom_value.value, options.merge(:id => tag_id, :size => 10)) +
+          view.calendar_for(tag_id)
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        view.text_field_tag(tag_name, value, options.merge(:id => tag_id, :size => 10)) +
+          view.calendar_for(tag_id) +
+          bulk_clear_tag(view, tag_id, tag_name, custom_field, value)
+      end
+
       def query_filter_options(custom_field, query)
         {:type => :date}
       end
@@ -247,6 +290,20 @@ module Redmine
         [[::I18n.t(:general_text_Yes), '1'], [::I18n.t(:general_text_No), '0']]
       end
 
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        view.hidden_field_tag(tag_name, '0') +
+          view.check_box_tag(tag_name, '1', custom_value.true?, options.merge(:id => tag_id))
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        view.select_tag(tag_name,
+          view.options_for_select([
+            [l(:label_no_change_option), ''],
+            [l(:general_text_Yes), '1'],
+            [l(:general_text_No), '0']], value),
+          options.merge(:id => tag_id))
+      end
+
       def query_filter_options(custom_field, query)
         {:type => :list, :values => possible_values_options(custom_field)}
       end
@@ -254,6 +311,33 @@ module Redmine
 
     class List < Base
       self.multiple_supported = true
+
+      def edit_tag(view, tag_id, tag_name, custom_value, options={})
+        blank_option = ''.html_safe
+        unless custom_value.custom_field.multiple?
+          if custom_value.custom_field.is_required?
+            unless custom_value.custom_field.default_value.present?
+              blank_option = view.content_tag('option', "--- #{l(:actionview_instancetag_blank_option)} ---", :value => '')
+            end
+          else
+            blank_option = view.content_tag('option')
+          end
+        end
+        options_tags = blank_option + view.options_for_select(possible_values_options(custom_value.custom_field, custom_value.customized), custom_value.value)
+        s = view.select_tag(tag_name, options_tags, options.merge(:id => tag_id, :multiple => custom_value.custom_field.multiple?))
+        if custom_value.custom_field.multiple?
+          s << view.hidden_field_tag(tag_name, '')
+        end
+        s
+      end
+
+      def bulk_edit_tag(view, tag_id, tag_name, custom_field, objects, value, options={})
+        opts = []
+        opts << [l(:label_no_change_option), ''] unless custom_field.multiple?
+        opts << [l(:label_none), '__none__'] unless custom_field.is_required?
+        opts += possible_values_options(custom_field, objects)
+        view.select_tag(tag_name, view.options_for_select(opts, value), options.merge(:multiple => custom_field.multiple?))
+      end
 
       def query_filter_options(custom_field, query)
         {:type => :list_optional, :values => possible_values_options(custom_field, query.project)}
@@ -296,8 +380,13 @@ module Redmine
       add 'user'
 
       def possible_values_options(custom_field, object=nil)
-        if object.respond_to?(:project) && object.project
+        if object.is_a?(Array)
+          projects = object.map {|o| o.respond_to?(:project) ? o.project : nil}.compact.uniq
+          projects.map {|project| possible_values_options(custom_field, project)}.reduce(:&) || []
+        elsif object.respond_to?(:project) && object.project
           object.project.users.sort.collect {|u| [u.to_s, u.id.to_s]}
+        else
+          []
         end
       end
     end
@@ -306,8 +395,13 @@ module Redmine
       add 'version'
 
       def possible_values_options(custom_field, object=nil)
-        if object.respond_to?(:project) && object.project
+        if object.is_a?(Array)
+          projects = object.map {|o| o.respond_to?(:project) ? o.project : nil}.compact.uniq
+          projects.map {|project| possible_values_options(custom_field, project)}.reduce(:&) || []
+        elsif object.respond_to?(:project) && object.project
           object.project.shared_versions.sort.collect {|u| [u.to_s, u.id.to_s]}
+        else
+          []
         end
       end
     end
