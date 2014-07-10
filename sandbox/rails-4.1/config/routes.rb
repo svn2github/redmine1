ebda2f65059c9b52603a5897fa38aaab861b368a
jplang <jplang@e93f8b46-1217-0410-a6f0-8f06a7374b81>
2014-07-10 21:25:26 +0000

    Config files cleanup.
    git-svn-id: http://svn.redmine.org/redmine@13274 e93f8b46-1217-0410-a6f0-8f06a7374b81

--- sandbox/rails-4.1/config/routes.rb
+++ sandbox/rails-4.1/config/routes.rb
@@ -15,7 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-RedmineApp::Application.routes.draw do
+Rails.application.routes.draw do
   root :to => 'welcome#index', :as => 'home'
 
   match 'login', :to => 'account#login', :as => 'signin', :via => [:get, :post]
